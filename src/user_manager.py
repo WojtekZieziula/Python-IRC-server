7d9a7dae9601e4c288a1a59536b149da3c2dc253
WojtekZieziula <wojciech.zieziula@gmail.com>
2026-01-04 22:20:03 +0100

    added case-insensitivity

--- src/user_manager.py
+++ src/user_manager.py
@@ -18,19 +18,27 @@ class UserManager:
         self.logger: logging.Logger
 
     def add_user(self, nickname: str, session: "ClientSession") -> None:
-        self.users[nickname] = session
-        self.logger.info(f"User added: {nickname}")
+        if self.is_nick_taken(nickname):
+            raise ValueError(f"Nickname '{nickname.lower()}' is already in use!")
+
+        self.users[nickname.lower()] = session
+        self.logger.info(f"User added: {nickname.lower()}")
+
+    def get_session(self, nickname: str) -> "ClientSession | None":
+        return self.users.get(nickname.lower())
 
     def remove_user(self, nickname: str) -> None:
-        if nickname in self.users:
-            del self.users[nickname]
+        low_nickname = nickname.lower()
+        if low_nickname in self.users:
+            del self.users[low_nickname]
             self.logger.info(f"User removed: {nickname}")
 
     def is_nick_taken(self, nickname: str) -> bool:
-        return nickname in self.users
+        return nickname.lower() in self.users
 
     def change_nick(self, old_nick: str, new_nick: str) -> None:
-        session = self.users[old_nick]
-        del self.users[old_nick]
-        self.users[new_nick] = session
-        self.logger.info(f"Nick changed: {old_nick} -> {new_nick}")
+        session = self.get_session(old_nick)
+        if session:
+            self.remove_user(old_nick)
+            self.add_user(new_nick, session)
+            self.logger.info(f"Nick changed: {old_nick.lower()} -> {new_nick.lower()}")
