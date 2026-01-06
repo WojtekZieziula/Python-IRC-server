e7328944d3b13354c3d5f4d898bb76d4073a6e2b
WojtekZieziula <wojciech.zieziula@gmail.com>
2026-01-06 05:44:52 +0100

    implemented JOIN, PART and PRIVMSG commands

--- src/commands.py
+++ src/commands.py
@@ -4,6 +4,7 @@ from src.config import ServerConfig
 from src.protocol import IRCMessage
 from src.session import ClientSession
 from src.user_manager import UserManager
+from src.channel_manager import ChannelManager
 
 
 class CommandHandler:
@@ -11,18 +12,23 @@ class CommandHandler:
         self.config = config
         self.logger = logging.getLogger(self.__class__.__name__)
         self.user_manager = UserManager()
+        self.channel_manager = ChannelManager()
 
     async def handle(self, session: ClientSession, msg: IRCMessage) -> None:
         command = msg.command
+        handlers = {
+            "PASS": self.handle_pass,
+            "NICK": self.handle_nick,
+            "USER": self.handle_user,
+            "QUIT": self.handle_quit,
+            "JOIN": self.handle_join,
+            "PRIVMSG": self.handle_privmsg,
+            "PART": self.handle_part,
+        }
 
-        if command == "PASS":
-            await self.handle_pass(session, msg)
-        elif command == "NICK":
-            await self.handle_nick(session, msg)
-        elif command == "USER":
-            await self.handle_user(session, msg)
-        elif command == "QUIT":
-            await self.handle_quit(session, msg)
+        handler = handlers.get(command)
+        if handler:
+            await handler(session, msg)
         else:
             self.logger.debug(f"Unknown command: {command}")
             await session.send_error("421", command, ":Unknown command")
@@ -78,6 +84,70 @@ class CommandHandler:
         session.username = msg.params[0]
         session.realname = msg.params[3]
         await self.check_registration(session)
+
+    async def handle_join(self, session: ClientSession, msg: IRCMessage) -> None:
+        if not msg.params:
+            await session.send_error("461", "JOIN", ":Not enough parameters")
+            return
+
+        channel_name = msg.params[0]
+        try:
+            channel = self.channel_manager.get_or_create_channel(channel_name)
+            channel.add_user(session)
+
+            join_msg = f":{session.nickname}!{session.username}@{session.host} JOIN {channel.name}"
+            await channel.broadcast(join_msg)
+
+            nicks = " ".join([m.nickname for m in channel.members if m.nickname])
+            await session.send_reply("353", session.nickname, "=", channel.name, f":{nicks}")
+            await session.send_reply("366", session.nickname, channel.name, ":End of /NAMES list")
+
+        except ValueError as e:
+            await session.send_error("403", channel_name, ":No such channel")
+
+    async def handle_part(self, session: ClientSession, msg: IRCMessage) -> None:
+        if not msg.params:
+            await session.send_error("461", "PART", ":Not enough parameters")
+            return
+
+        channel_name = msg.params[0]
+        channel = self.channel_manager.get_channel(channel_name)
+
+        if not channel:
+            await session.send_error("403", channel_name, ":No such channel")
+            return
+
+        if session not in channel.members:
+            await session.send_error("442", channel_name, ":You're not on that channel")
+            return
+
+        part_msg = f":{session.nickname} PART {channel.name}"
+        await channel.broadcast(part_msg)
+        channel.remove_user(session)
+
+    async def handle_privmsg(self, session: ClientSession, msg: IRCMessage) -> None:
+        if len(msg.params) < 2:
+            await session.send_error("411", ":No recipient given (PRIVMSG)")
+            return
+
+        target = msg.params[0]
+        content = msg.params[1]
+
+        if target.startswith("#"):
+            channel = self.channel_manager.get_channel(target)
+            if channel:
+                if session not in channel.members:
+                    await session.send_error("404", target, ":Cannot send to channel")
+                    return
+                await channel.broadcast(f":{session.nickname} PRIVMSG {target} :{content}", skip_user=session)
+            else:
+                await session.send_error("401", target, ":No such nick/channel")
+        else:
+            target_user = self.user_manager.get_session(target)
+            if target_user:
+                await target_user.send_reply(f":{session.nickname} PRIVMSG {target} :{content}")
+            else:
+                await session.send_error("401", target, ":No such nick/channel")
 
     async def handle_quit(self, session: ClientSession, msg: IRCMessage) -> None:
         reason = msg.params[0] if msg.params else "Client Quit"
