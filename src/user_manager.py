f0ca4c408a5b0a8f0d37ad5c7483abc975d6df6a
WojtekZieziula <wojciech.zieziula@gmail.com>
2026-01-07 19:38:43 +0100

    added _irc_lower method

--- src/user_manager.py
+++ src/user_manager.py
@@ -17,28 +17,38 @@ class UserManager:
         self.users: dict[str, "ClientSession"]
         self.logger: logging.Logger
 
-    def add_user(self, nickname: str, session: "ClientSession") -> None:
-        if self.is_nick_taken(nickname):
-            raise ValueError(f"Nickname '{nickname.lower()}' is already in use!")
+    @staticmethod
+    def _irc_lower(nick: str) -> str:
+        return nick.lower().replace("[", "{").replace("]", "}").replace("\\", "|")
 
-        self.users[nickname.lower()] = session
-        self.logger.info(f"User added: {nickname.lower()}")
+    def add_user(self, nickname: str, session: "ClientSession") -> None:
+        low_nickname = self._irc_lower(nickname)
+
+        if self.is_nick_taken(nickname):
+            raise ValueError(f"Nickname '{low_nickname}' is already in use!")
+
+        self.users[low_nickname] = session
+        self.logger.info(f"User added: {low_nickname}")
 
     def get_session(self, nickname: str) -> "ClientSession | None":
-        return self.users.get(nickname.lower())
+        return self.users.get(self._irc_lower(nickname))
 
     def remove_user(self, nickname: str) -> None:
-        low_nickname = nickname.lower()
+        low_nickname = self._irc_lower(nickname)
         if low_nickname in self.users:
             del self.users[low_nickname]
             self.logger.info(f"User removed: {nickname}")
 
     def is_nick_taken(self, nickname: str) -> bool:
-        return nickname.lower() in self.users
+        return self._irc_lower(nickname) in self.users
 
     def change_nick(self, old_nick: str, new_nick: str) -> None:
         session = self.get_session(old_nick)
         if session:
+            low_new = self._irc_lower(new_nick)
+            if low_new in self.users and low_new != self._irc_lower(old_nick):
+                raise ValueError(f"Nickname '{low_new}' is already in use!")
+
             self.remove_user(old_nick)
             self.add_user(new_nick, session)
-            self.logger.info(f"Nick changed: {old_nick.lower()} -> {new_nick.lower()}")
+            self.logger.info(f"Nick changed: {self._irc_lower(old_nick)} -> {low_new}")
