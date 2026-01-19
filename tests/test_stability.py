6c2dae75576d213bb343863dc90dfc69fbf94837
Kacper Siemionek <siemionek.kacper22@gmail.com>
2026-01-19 21:30:50 +0100

    add stability tests

---
+++ tests/test_stability.py
@@ -0,0 +1,32 @@
+import pytest
+import asyncio
+
+from IRCClient import IRCClient
+
+
+@pytest.mark.asyncio
+async def test_invalid_data_and_long_line(running_server: int) -> None:
+    port = running_server
+    client = IRCClient(port, "Ziutek")
+    try:
+        await client.connect()
+        await client.send("\x00\xff\xfeINVALID\r\n")
+        long_message = "PRIVMSG #general :" + "A" * 10000
+        await client.send(long_message)
+        await client.send("NICK Ziutek2")
+        response = await client.wait_for_message("NICK")
+        assert "NICK" in response
+    finally:
+        await client.close()
+
+@pytest.mark.asyncio
+async def test_abrupt_client_disconnect(running_server: int) -> None:
+    port = running_server
+    client = IRCClient(port, "Ziutek")
+    try:
+        await client.connect()
+        if client.writer:
+            client.writer.transport.abort()
+        await asyncio.sleep(0.1)
+    finally:
+        await client.close()
